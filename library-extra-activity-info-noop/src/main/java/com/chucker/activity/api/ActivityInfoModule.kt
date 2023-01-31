e3056a4c50c96447aa75830edcf3ea3122f1daa3
Michael <michael.michael@tiket.com>
2023-01-31 09:52:18 +0700

    fix activity info module noop

--- library-extra-activity-info-noop/src/main/java/com/chucker/activity/api/ActivityInfoModule.kt
+++ library-extra-activity-info-noop/src/main/java/com/chucker/activity/api/ActivityInfoModule.kt
@@ -1,12 +1,14 @@
 package com.chucker.activity.api
 
+import android.app.Activity
 import android.app.Application
 import android.content.Context
 import com.chuckerteam.chucker.api.extramodule.ChuckerExtraModule
 
 class ActivityInfoModule(
-        app: Application,
-        enableNotification: Boolean = true
+    app: Application,
+    enableNotification: Boolean = true,
+    additionalInfo: ((Activity) -> Map<String, String>?)? = null
 ) : ChuckerExtraModule {
 
     override val moduleName: String = "Activity Info"
